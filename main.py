7434632d6f647e74dfbdfc5f05254f616d48dcce
Pavel <pavellatinov@gmail.com>
2026-04-12 00:44:05 +0500

    debug print

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+print("СТАРТ ФАЙЛА")
+
+import os
+from telegram import Update
+from telegram.ext import Application, MessageHandler, filters, ContextTypes
+
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     print("ПОЛУЧИЛ СООБЩЕНИЕ")
 
@@ -8,3 +16,18 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
         return
 
     await update.message.reply_text("Я живой")
+
+
+def main():
+    print("ЗАШЛИ В MAIN")
+
+    app = Application.builder().token(TOKEN).build()
+
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+
+    print("Бот запущен...")
+    app.run_polling()
+
+
+if __name__ == "__main__":
+    main()
