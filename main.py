6a7ef59f3de05695c519b86aa3be00b5ef883ca8
Pavel <pavellatinov@gmail.com>
2026-04-12 23:32:33 +0500

    fix conflict

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def main():
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
     print("Бот запущен...")
-    app.run_polling()
+    app.run_polling(drop_pending_updates=True)
 
 if __name__ == "__main__":
     main()
